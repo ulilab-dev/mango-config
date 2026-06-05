0900f2a2d9cbefafec97f27179346d71100402ce
ulilab-dev <ullia5733@gmail.com>
2026-06-05 14:02:34 +0700

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -3,7 +3,7 @@
 chmod +x waybar/toggle_waybar.sh &&
 chmod +x hypr/wallpaper-pick.sh &&
 
-cp -r dunst/ fastfetch/ fish/ mango/ kitty/ rofi/ waybar/ wlogout/ ~/.config && 
+cp -r dunst/ fastfetch/ fish/ kitty/ mango/ rofi/ waybar/ wlogout/ ~/.config && 
 cp -r Simp1e/ ~/.local/share/icons &
 
 if [ $? -eq 0 ]; then
